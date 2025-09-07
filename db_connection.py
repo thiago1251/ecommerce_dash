faaa970b24c61a35b1866de3565dd3e1f6786944
Santiago Tapia <115585014+thiago1251@users.noreply.github.com>
2025-09-06 22:52:24 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from config import Config
 
 engine = create_engine(
-    f"postgresql+psycopg2://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}",
+    f"postgresql+psycopg://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}",
     pool_pre_ping=True,
     pool_size=5,
     max_overflow=10,
